acfc823fcd9d162a0bb611ee75d390918d4ff8e5
miknayr <teamry4n@gmail.com>
2021-05-06 15:32:25 -0700

    solutions to JS loops lab

---
+++ solution.js
@@ -0,0 +1,66 @@
+// get even lab - done
+
+// for (i=0; i < 200;) {
+//     console.log(i);
+//     i = i + 2;
+// } 
+
+// for (i=0; i<= 200; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// Excited kitten
+
+// let catChat = [
+//     "human...why you taking pictures of me?...",
+//     "the catnip made me do it...",
+//     "meow?",
+//     "why does the red dot always get away..."
+// ];
+
+// for (let i=0; i<=10; i++) {
+//     if (i % 2 == 0) {
+//     console.log(catChat[Math.floor(Math.random() * catChat.length)])
+//     } else if (i % 2 == 1) {
+//         console.log ("love me,pet me! HSSSSSS");
+//     }
+// }
+
+
+
+// thermostat - done
+
+// let currentTemp = Math.floor(Math.random() * 100)
+
+// let desiredTemp = 72
+// console.log(`The current temperature is ${currentTemp}F`)
+
+
+// while (currentTemp !== desiredTemp) {
+//     if (currentTemp < desiredTemp) {
+//         currentTemp++
+//         console.log (`The temp rising to ${currentTemp}.`)
+//     } else if (currentTemp > desiredTemp) {
+//         currentTemp--
+//         console.log(`The temp is dropping to ${currentTemp}.`)
+//     }else {
+//         console.log(`The current temperature is ${currentTemp}.`)
+//     }
+// }
+
+// fizz buzz 
+
+for (let i = 0; i < 100; i++) {
+    if (i % 3 == 0 && i % / 5 == 0) {
+        console.log('fizzbuzz')
+    } else if (i % 3 == 0) {
+        console.log('fizz')
+    } else if (i % 5 == 0) {
+        console.log('buzz')
+    } else {
+        console.log(i)
+    }
+}
+    
